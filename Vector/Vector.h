9454c24ee68ecc0cdb05083d773aea4579aa5961
Amour132 <1046692639@qq.com>
2018-11-20 21:26:24 +0800

    完善了默认函数，增加了部分成员函数

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,5 +1,6 @@
 #pragma once 
 
+#include <assert.h>
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -18,6 +19,16 @@ public:
   }
 
   iterator end()
+  {
+    return _finish;
+  }
+
+  const iterator cbegin()const 
+  {
+    return _start;
+  }
+  
+  const iterator cend()const 
   {
     return _finish;
   }
@@ -49,11 +60,19 @@ public:
            ,_endofstorage(NULL)
   {}
 
-  Vector(const Vector& v)
+  Vector(const Vector<T>& v):_start(NULL)
+                             ,_finish(NULL)
+                             ,_endofstorage(NULL)
   {
-    v._start = (T*)memcpy(&v._start,&_start,sizeof(T)*Capacity());
-    v._finish = v._start + Size();
-    v._endofstorage = v._start + Capacity();
+    Reserve(v.Capacity());
+    iterator it = begin();
+    const iterator vit = cbegin();
+    while(vit != cend())
+    {
+      *it++ = *vit++;
+    }
+    _finish = _start + v.Size();
+    _endofstorage = _start + v.Capacity();
   }
 
   ~Vector()
@@ -79,12 +98,107 @@ public:
   }
 
   //成员函数
-  void Reserve(size_t n);
-  void Resize(size_t n,const T& val = T());
-  void PushBack(const T& x);
-  void Pop();
-  void Insert(iterator pos,const T& x);
-  iterator Earse(iterator pos);
+  void Reserve(size_t n)
+  {
+    if(n > Capacity())
+    {
+      size_t size = Size();
+      T* tmp = new T[n];
+      if(_start)
+      {
+        for(size_t i=0; i<size; i++)
+        {
+          tmp[i] = _start[i];
+        }
+      }
+      _start = tmp;
+      _finish = _start + size;
+      _endofstorage = _start + n;
+    }
+  }
+
+  void Resize(size_t n,const T& value = T())
+  {
+    if(n<Capacity())
+    {
+      _finish = _start + n;
+      return;
+    }
+    if(n>Capacity())
+    {
+      Reserve(n);
+    }
+    iterator it = _finish;
+    iterator _finish = _start + n;
+    while(it != _finish)
+    {
+      *it = value;
+      it++; 
+    }
+  }
+
+  void PushBack(const T& x)
+  {
+    Insert(end(),x);
+  }
+
+  void Pop()
+  {
+    Earse(end()-1);
+  }
+
+  iterator Insert(iterator pos,const T& x)
+  {
+    assert(pos<=_finish);
+
+    if(_endofstorage == _finish)
+    {
+      size_t size = Size();
+      size_t newcapacity;
+      if(Capacity() == 0)
+      {
+        newcapacity = 1;
+      }
+      else
+      {
+        newcapacity = 2*Capacity();
+      }
+      Reserve(newcapacity);
+      pos = _start + size;
+    }
+    iterator end = _finish -1;
+    while(end >= pos)
+    {
+      *(end+1) = *end;
+      end--;
+    }
+    *pos = x;
+    ++_finish;
+    return pos;
+  }
+
+  iterator Earse(iterator pos)
+  {
+    iterator begin = pos + 1;
+    while(begin != _finish)
+    {
+      *(begin-1) = *begin;
+      begin++;
+    }
+    _finish--;
+    return pos;
+  }
+
+  void print()
+  {
+    iterator it = begin();
+    while(it != end())
+    {
+      cout<<*it<<"-";
+      it++;
+    }
+    cout<<endl;
+  }
   
 private:
   T* _start;
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -3,6 +3,15 @@
 int main()
 {
    djh::Vector<int> v1;
-
+   v1.PushBack(1);
+   v1.PushBack(2);
+   v1.PushBack(3);
+   v1.Pop();
+   djh::Vector<string> v2;
+   v2.PushBack("hello");
+   v2.PushBack(" world");
+   v2.Pop();
+   v1.print();
+   v2.print();
    return 0;
 }
